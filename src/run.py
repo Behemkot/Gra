a1393b6a879521779faf27e29ed670f9d37949af
Szymon Walter <walter.szymon.98@gmail.com>
2018-05-12 14:48:10 +0200

    break, unbreak, sprinkle some bugs and debug half of them

--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 import pygame as g
 import sys
 from player import Player
+from platform import Platform
 from physics import World
 
 class Game(object):
@@ -8,10 +9,10 @@ class Game(object):
         # CONFIG
         self.resolution = (600, 400)
         self.tps = 60.0
-        self.jump_force = 1
-        self.move_speed = 1
-        self.max_speed = 1
-        self.gravity = 1
+        self.jump_force = 45000
+        self.move_speed = 12000
+        self.max_speed = 20000
+        self.gravity = 1800
 
         # Inicjowanie
         g.init()
@@ -21,10 +22,10 @@ class Game(object):
         self.tps_delta = 0.0
 
         self.player = Player(self)
-        self.platform = Platform(self)
 
         self.world = World()
         self.world.add_body(self.player)
+        self.world.add_body(Platform(self))
 
     def run(self):
         while True:
@@ -36,24 +37,22 @@ class Game(object):
             # tiki
             self.tps_delta += self.tps_clock.tick() / 1000.0
             while self.tps_delta > 1 / self.tps:
+                self.update(self.tps_delta)
                 self.tps_delta -= 1 / self.tps
-                self.update()
 
             # rysowanie
             self.screen.fill((0, 0, 0))
             self.draw()
             g.display.update()
 
-    def update(self):
-        self.world.update(1)
+    def update(self, dt):
+        self.world.update(dt)
 
     def draw(self):
-        self.player.draw()
-        self.platform.draw()
+        for obj in self.world.bodies:
+            obj.draw()
 
 
 if __name__ == "__main__":
     game = Game()
-    player = Player(game)
-    platform = Platform(game)
     game.run()
