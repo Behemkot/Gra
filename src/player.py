786a0f22015b5aebe85b063a6d1045b50faf951b
Szymon Walter <walter.szymon.98@gmail.com>
2018-06-05 20:37:51 +0200

    add enemy collision and health

--- src/player.py
+++ src/player.py
@@ -2,7 +2,24 @@ import pygame as g
 from pygame.math import Vector2
 from physics import Body
 from physics import Bbox
+from enemy import Enemy
 
+
+INVINCIBILITY = 1500 # 1.5s
+
+def chandler(collision):
+    player = None
+    bounce = None
+    if isinstance(collision.a.body, Enemy):
+        player = collision.b.body
+        bounce = Vector2(collision.intersection[0], collision.intersection[1])
+    elif isinstance(collision.b.body, Enemy):
+        player = collision.a.body
+        bounce = Vector2(-collision.intersection[0], -collision.intersection[1])
+    if player.invincible <= 0:
+        self.health -= 1
+        player.apply_force(bounce)
+        player.invincible = INVINCIBILITY 
 
 class Player(Body):
     def __init__(self, game):
@@ -11,11 +28,14 @@ class Player(Body):
         position_y = 250
         self.width = 50
         self.height = 50
+        self.health = 3
+        self.invincible = 0
 
         self.on_ground = False
 
         shape = Bbox(position_x, position_y, self.width, self.height)
         super(Player, self).__init__(position_x, position_y, shape, self.game.gravity, 0.8)
+        shape.on_collide(chandler)
 
     def jump(self):
         if self.on_ground:
@@ -45,6 +65,9 @@ class Player(Body):
             self.velocity[0] = self.game.max_speed
         if self.velocity[0] < -self.game.max_speed:
             self.velocity[0] = -self.game.max_speed
+
+        if self.invincible > 0:
+            self.invincible -= dt
 
     def draw(self):
         self.game.camera.draw(g.draw.ellipse, (0, 255, 0), self.position, (self.width, self.height))
